2783c6a119b9774bf405e143144feddf13a1d368
jsayshi <67714254+jsayshi@users.noreply.github.com>
2020-08-02 22:15:33 -0400

    Added comments
    I'm new but I was told comments are important.

--- search_bot.py
+++ search_bot.py
@@ -1,24 +1,27 @@
 import tweepy
 import time
 
+#You will need to get these from twitter and keep them SECRET! Apply for a developer account and follow the instructions.
 consumer_key = 
 consumer_secret = 
 key = 
 secret = 
 
+#The tweepy documentation is a must read and you will find the code below there.
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 
 api = tweepy.API(auth)
 
-
+#Input hashtag you are targeting.
 hashtag = 
+
+#Input amount of tweets you want tyo engage with. Keep it reasonable to stay within rate limits.
 tweetnumber = 
 
 tweets = tweepy.Cursor(api.search, hashtag).items(tweetnumber)
 
-
-
+#Function to engage user's twitterbot with other twitter users. The tweepy documentation shows you different ways to engage.
 def search_bot():
 	for tweet in tweets:
 		try:
@@ -28,6 +31,5 @@ def search_bot():
 		except tweepy.TweepError as e:
 			print(e.reason)
 			
-		
-
+#Call function. Rinse. Repeat. Enjoy.
 search_bot()
